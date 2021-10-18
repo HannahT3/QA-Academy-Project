1dedee48d171836525964769b76333186f0ebcbc
HannahT3 <hannah.treherne@hotmail.co.uk>
2021-10-18 15:25:13 +0100

    programme commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using Examples.ConsoleAppExample.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NBS_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
