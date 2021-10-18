8460db382ce9440c7e7782018ede4ec000c0f39c
HannahT3 <hannah.treherne@hotmail.co.uk>
2021-10-18 15:44:11 +0100

    adding first menu

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿
- 
+﻿using System;
+using System.Data;
+//using Examples.ConsoleAppExample.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBS_Project.Menus;
 
+
+namespace NBS_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMenus.EnterMenu();
+        }
+
+
+
+
+    }
+}
