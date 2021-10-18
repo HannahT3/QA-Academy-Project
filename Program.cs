cdcd292b2f53fcc23c3b96bd69cf7e0cd13c3c63
HannahT3 <hannah.treherne@hotmail.co.uk>
2021-10-18 15:41:51 +0100

    adding first menu

--- Program.cs
+++ Program.cs
@@ -1,18 +1,3 @@
-﻿using System;
-//using Examples.ConsoleAppExample.Controllers;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+ 
 
-
-namespace NBS_Project
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
