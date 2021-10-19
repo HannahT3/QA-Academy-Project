726ac6ad1c57c5bec0a651e7ee7e16648a7519db
HannahT3 <hannah.treherne@hotmail.co.uk>
2021-10-19 11:39:20 +0100

    adding create controller

---
+++ QA Project 1/QA Project 1/Controllers/SalesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QA_Project_1.Controllers
+{
+    class SalesController
+    {
+        internal void Create()
+        {
+            Console.WriteLine("What is the product name?");
+            Console.Write("> ");
+            //user input of product name
+            string name = Console.ReadLine();
+            Console.WriteLine("What is the quantity of the item?");
+            Console.Write("> ");
+            int quantity = Console.ReadLine();
+            Console.WriteLine("What is the price of the product?");
+            Console.Write("> ");
+            double price = Console.Read();
+            DateTime saleDate = DateTime.Now;
+        }
+    }
+}
